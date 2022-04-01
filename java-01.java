e6fd8cb4fad290741ddc0d313a22c11dd95ba6fe
André Souza <aljs.junca@gmail.com>
2022-04-01 20:44:01 -0300

    Populando o Repositório de Java
    Estou estudando Java pelo Alura

---
+++ java-01.java
@@ -0,0 +1,7 @@
+/* Olá Mundo com Java*/ 
+
+public class estudo01 {
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+	}
+}
---
+++ java-02.java
@@ -0,0 +1,7 @@
+/* Usando uma Variável do Tipo String */
+public class estudo02 {
+	public static void main (String[] args) {
+		String nome = "André";
+		System.out.println("Olá " + nome );
+	}
+}
