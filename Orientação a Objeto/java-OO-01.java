105f4f2634c5be631a6aeed599377f55c5d43621
André Souza <aljs.junca@gmail.com>
2022-04-02 19:12:49 -0300

    Update java-OO-01.java

--- Orientação a Objeto/java-OO-01.java
+++ Orientação a Objeto/java-OO-01.java
@@ -30,7 +30,7 @@ public class CriaConta {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
 		System.out.println("Saldo da primeira Conta " + "R$ " +primeiraConta.saldo);
-		primeiraConta.saldo += 100; 
+		primeiraConta.saldo += 100;
 		System.out.println("Saldo da primeira Conta " + "R$ " +primeiraConta.saldo);
 
 		/* Criando Segunda Conta */
@@ -40,4 +40,32 @@ public class CriaConta {
 		segundaConta.saldo += 100;
 		System.out.println("Saldo da Segunda Conta " + "R$ " + segundaConta.saldo);
 	}
+}
+
+/* Primeiro Método Contas.java */
+
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+
+
+	public void deposita(double valor) {
+		this.saldo = this.saldo + valor;
+	}
+}
+
+/* Testando Método */
+
+
+public class testaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaDoAndre = new Conta();
+		contaDoAndre.saldo = 100;
+		contaDoAndre.deposita(50);
+		System.out.println(contaDoAndre.saldo);
+	}
+
 }
