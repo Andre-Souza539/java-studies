5297262fa70398fe585f797abac032b19c47fd1f
André Souza <aljs.junca@gmail.com>
2022-04-02 15:09:25 -0300

    portifolio java

---
+++ Java Basico/java-05.java
@@ -0,0 +1,21 @@
+/*  verificar maior idade */
+
+
+public class verificaMaiorIdade {
+
+	public static void main(String[] args) {
+
+		System.out.println("Testando Condicionais");
+		System.out.println();
+
+		int idade = 17;
+		System.out.println("Sua idade é: " + idade);
+
+		if (idade >= 18) {
+			System.out.println("Você é Maior de Idade");
+		} else  {
+			System.out.println("Você é menor de Idade");
+		}
+
+	}
+}
