ad4b50f889abf6addca1b235ab999da8dd714def
André Souza <aljs.junca@gmail.com>
2022-04-04 14:23:57 -0300

    Update java-OO-01.java

--- Orientação a Objeto/java-OO-01.java
+++ Orientação a Objeto/java-OO-01.java
@@ -138,3 +138,43 @@ public class objetoCliente {
 
 	}
 }
+
+/* Atributo Privado */
+
+public class Conta {
+	private double saldo;
+	int agencia;
+	int numero;
+	Cliente titular;
+
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if(this.saldo >= valor ) {
+			this.saldo -= valor;
+			return true;
+		}
+		System.out.println("Saldo Insuficiente");
+		return false;
+
+	}
+
+	public boolean transferir(double valor, Conta destino) {
+
+		if(this.saldo >= valor ) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		System.out.println("Saldo Insuficiente");
+		return false;
+	}
+	/* Agora será necessário o uso do Método pegaSaldo() para verificar o valor
+	do saldo em uma conta  */
+	public double pegaSaldo() {
+		return this.saldo;
+	}
+}
